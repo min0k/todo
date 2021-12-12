70c53dd55ddc375454c4de4baa2249f95994fcfd
min0k <mino.kim@minokim.com>
2021-12-12 10:41:58 -0800

    create addNewTodo as module

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("log");
+import { addNewTodo } from "./modules/addNewTodo.js";
+
+
+const todoController = () => {
+
+    //controls and decides how data is displayed
+
+    //handles events and is the 
+    //mediator between the view and model.
+}
+
+
+//-----------------------------------------
+
+
+
+const todoView = () => {
+    addEventlisteners();
+    //changes UI/DOM
+}
+
+function addEventlisteners() {
+    const createTodoButton = document.querySelector("#create-todo");
+    createTodoButton.addEventListener("click", addNewTodo)
+}
+
+todoView();
+
+
+//-----------------------------------------
+const todoModel = () => {
+    //data logic/backend
+}
