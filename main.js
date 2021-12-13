2fe498b58792ce9bde70130e553310fc575e21d7
min0k <mino.kim@minokim.com>
2021-12-12 20:20:14 -0800

    basic functionality

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("hello");
+
+const todoSubmitButtonEl = document.querySelector("#new-todo-submit");
+const todoInputEl = document.querySelector("#new-todo-text");
+const todoBody = document.querySelector("#todo-container");
+
+todoSubmitButtonEl.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!todoInputEl.value) return;
+
+    const todoContainer = document.createElement("div");
+    todoContainer.className = "todo";
+    
+    const todoTextEl = document.createElement("p");
+    
+    const todoCheckboxEl = document.createElement("input");
+    todoCheckboxEl.type = "checkbox";
+    
+    const todoDeleteButtonEl = document.createElement("button");
+    todoDeleteButtonEl.textContent = ("delete");
+
+    todoTextEl.textContent = todoInputEl.value;
+    todoInputEl.value = "";
+
+    todoContainer.appendChild(todoTextEl);
+    todoContainer.appendChild(todoCheckboxEl);
+    todoContainer.appendChild(todoDeleteButtonEl);
+    
+    todoBody.appendChild(todoContainer);
+
+    todoDeleteButtonEl.addEventListener("click", (e) => {
+        todoDeleteButtonEl.parentElement.remove();
+    })
+
+    todoCheckboxEl.addEventListener("click", (e) => {
+        if (todoCheckboxEl.checked) {
+            todoTextEl.classList.add("todo-done");
+        } else {
+            todoTextEl.classList.remove("todo-done");
+        }
+    })
+
+})
+
