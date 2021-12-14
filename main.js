8a7d53ec25a7626d27ec8a5641655577574f5477
min0k <mino.kim@minokim.com>
2021-12-13 22:11:06 -0800

    add notes for next work session

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import { renderTodos } from "./modules/renderTodo.mjs";
-import { todos, updateTodosArray } from "./modules/todoArray.mjs";
+//// NEXT STEPS
+//// 1. Store todo array in local storage
+//// 2. render todos on window load
 
+import { renderTodos } from "./modules/renderTodo.mjs";
+import { todos } from "./modules/todoArray.mjs";
 
 const todoSubmitButtonEl = document.querySelector("#new-todo-submit");
 const todoInputEl = document.querySelector("#new-todo-text");
@@ -18,11 +21,10 @@ const addTodoToArray = () => {
     const todoFactory = (todoText, status) => {
         return {todoText, status}
     }
-
     const newTodo = todoFactory(todoInputEl.value, false);
-
     todos.push(newTodo);
     console.log(todos);
- 
 }
+
+
 
--- modules/todoArray.mjs
+++ modules/todoArray.mjs
@@ -1,3 +1,7 @@
+//// Make todos a private array, make it a closure. 
+//// create todo as a private variable in a function, return a function that
+//// adds the new object to it
+
 export let todos = [];
 
 export const updateTodosArray = (deleteMe) => {
